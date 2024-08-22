a9d887efe4f7d98822ec92352d54dbc7624d3680
cmthPeerapon <peerapon.boonkaweenapanon@classmethod-thailand.com>
2024-08-22 16:06:51 +0700

    code deploy ec2 w/ buildx

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,5 +4,7 @@ echo 'run after_install.sh: ' >>/home/ec2-user/deploy.log
 
 cd /var/www/html
 
-echo 'docker build -t tic-tac-toe .' >>/home/ec2-user/deploy.log
-docker build -t tic-tac-toe . >>/home/ec2-user/deploy.log
+echo 'docker buildx create --driver=docker-container --use' >>/home/ec2-user/deploy.log
+docker buildx create --driver=docker-container --use >>/home/ec2-user/deploy.log
+echo 'docker buildx build --cache-from=type=local,src=cache --cache-to=type=local,dest=cache -t tic-tac-toe --load .' >>/home/ec2-user/deploy.log
+docker buildx build --cache-from=type=local,src=cache --cache-to=type=local,dest=cache -t tic-tac-toe --load . >>/home/ec2-user/deploy.log
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,15 +7,13 @@ cd /var/www/html
 
 echo 'yum update -y' >>/home/ec2-user/deploy.log
 yum update -y >>/home/ec2-user/deploy.log
-echo 'yum install git docker -y' >>/home/ec2-user/deploy.log
-yum install git docker -y >>/home/ec2-user/deploy.log
+echo 'yum install docker -y' >>/home/ec2-user/deploy.log
+yum install docker -y >>/home/ec2-user/deploy.log
 
 echo 'systemctl start docker' >>/home/ec2-user/deploy.log
 systemctl start docker >>/home/ec2-user/deploy.log
 echo 'systemctl enable docker' >>/home/ec2-user/deploy.log
 systemctl enable docker >>/home/ec2-user/deploy.log
 
-# echo 'git init' >> /home/ec2-user/deploy.log
-# git init >> /home/ec2-user/deploy.log
-# echo 'git pull https://github.com/cmthPeerapon/tic-tac-toe.git' >> /home/ec2-user/deploy.log
-# git pull https://github.com/cmthPeerapon/tic-tac-toe.git >> /home/ec2-user/deploy.log
+echo 'docker buildx install' >>/home/ec2-user/deploy.log
+docker buildx install >>/home/ec2-user/deploy.log
