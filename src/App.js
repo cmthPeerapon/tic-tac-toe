88ba92face8b50a1c263eff108fef525f8b2bf32
cmthPeerapon <peerapon.boonkaweenapanon@classmethod-thailand.com>
2024-10-17 16:58:40 +0700

    add unused var

--- src/App.js
+++ src/App.js
@@ -91,6 +91,7 @@ function Version() {
 }
 
 function App() {
+  const [componentDidMount, setComponentDidMount] = useState(false);
   const [currentBoardStatus, setCurrentBoardStatus] = useState(
     Array(3).fill(Array(3).fill(null))
   );
