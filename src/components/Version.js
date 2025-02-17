8574280b3b0282d0cef679bc666ad4ae4f307583
cmthPeerapon <peerapon.boonkaweenapanon@classmethod-thailand.com>
2025-02-17 14:42:16 +0700

    wip test workflow

--- src/components/Version.js
+++ src/components/Version.js
@@ -1,7 +1,7 @@
 export function Version() {
   return (
     <div className="absolute bottom-0 right-0 select-none">
-      <span role="text" aria-label="version">
+      <span role="textbox" aria-label="version">
         Version 1.1
       </span>
     </div>
